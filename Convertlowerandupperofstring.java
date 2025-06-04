deba1f37dc0e578eb4035832d4012fba0fd3fa9c
gowthami-koduru <gowthamikoduru65@gmail.com>
2025-06-04 15:06:22 +0530

    Add files via upload

---
+++ Convertlowerandupperofstring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Convertlowerandupperofstring
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String a=sc. nextLine();
+	String u=" ";
+	String l=" ";
+    System.out.println(a.toLowerCase());
+    System.out.println(a.toUpperCase());
+	}
+}
